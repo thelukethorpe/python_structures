8e7fc0ec6b2fabcf740a8e7baf620faac83dfff2
Luke Waters-Thorpe <thelukethorpe@outlook.com>
2026-08-13 21:11:38 +0100

    temp

--- src/filthyrat/structures/balanced_heap.py
+++ src/filthyrat/structures/balanced_heap.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from typing import cast
 
 type Comparator[T] = Callable[[T, T], int]
 
@@ -25,9 +26,8 @@ class BalancedHeap[T]:
             self._top = value
             return
 
-        cmp = self._cmp(value, self._top)
         larger_value: T
-        if cmp < 0:
+        if self._cmp(value, self._top) < 0:
             larger_value = self._top
             self._top = value
         else:
@@ -46,3 +46,49 @@ class BalancedHeap[T]:
             smaller_child = self._right
 
         smaller_child.add(larger_value)
+
+    def pop(self) -> T | None:
+        if self._top is None:
+            return None
+
+        self._size -= 1
+        top = self._top
+        child_with_smaller_top: BalancedHeap[T] | None
+        if self._left is None:
+            child_with_smaller_top = self._right
+        elif self._right is None:
+            child_with_smaller_top = self._left
+        elif self._cmp(cast(T, self._left._top), cast(T, self._right._top)) < 0:
+            child_with_smaller_top = self._left
+        else:
+            child_with_smaller_top = self._right
+
+        if child_with_smaller_top is None:
+            self._top = None
+            return top
+
+        self._top = child_with_smaller_top.pop()
+
+        child_with_larger_top: BalancedHeap[T] | None
+        if child_with_smaller_top == self._left:
+            child_with_larger_top = self._right
+        else:
+            child_with_larger_top = self._left
+
+        # TODO prune children
+        # TODO double check
+        # TODO is there a cleaner approach? (intermediate nodes, but less efficient - maybe on_empty callback)
+        # TODO test
+
+        if child_with_larger_top is None:
+            return top
+
+        # Rotate
+        if child_with_smaller_top._size - child_with_larger_top._size > 1:
+            top_to_rotate = cast(T, child_with_smaller_top.pop())
+            child_with_larger_top.add(top_to_rotate)
+        elif child_with_larger_top._size - child_with_smaller_top._size > 1:
+            top_to_rotate = cast(T, child_with_larger_top.pop())
+            child_with_smaller_top.add(top_to_rotate)
+
+        return top
